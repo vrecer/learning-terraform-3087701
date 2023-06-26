e84ec5306a345e1fdc9afbb050f21c20b243e206
David Vrečer <vrecer@gmail.com>
2023-06-26 11:41:12 +0200

    x

--- main.tf
+++ main.tf
@@ -22,8 +22,9 @@ module "vpc" {
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
